d3f574dca76558dc9d62d4c92e6d85f5802d0e24
Choe-Ji-Hwan <ghldtjd901@naver.com>
2023-05-26 11:19:26 +0900

    feat: create ImageTextCard using Composable and Preview

--- app/src/main/java/com/example/sandbox/MainActivity.kt
+++ app/src/main/java/com/example/sandbox/MainActivity.kt
@@ -3,44 +3,58 @@ package com.example.sandbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.sandbox.ui.theme.SandboxTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            SandboxTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ImageTextCard("Hello", "World")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ImageTextCard(firstValue: String, lastValue: String) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "Test Android Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(firstValue)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(lastValue)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+// Preview layer
+@Preview
 @Composable
-fun GreetingPreview() {
-    SandboxTheme {
-        Greeting("Android")
-    }
+fun PreviewTextCard() {
+    ImageTextCard(firstValue = "Hello", lastValue = "World")
 }
